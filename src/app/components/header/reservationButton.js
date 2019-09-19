64e41490a25dbadb77ae8ba0f8f1fe3df05193ad
Postas <postas87@mail.ru>
2019-09-19 21:20:39 +0300

    header_04

--- src/app/components/header/reservationButton.js
+++ src/app/components/header/reservationButton.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const ReservationButton = () => {
   return (
       <div className="header-reservation">
+          <a className={'header-reservation__link'} href={'#'}>
             Забронировать место
+          </a>
       </div>
   )
 };
