616bd4be348f41a70bdcc7a2acf91bc8ec452030
Postas <postas87@mail.ru>
2019-09-22 21:59:48 +0300

    content_06

--- src/app/components/content/main.js
+++ src/app/components/content/main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { PhotosessionInfo } from './photosessionInfo'
 import { PhotosessionInfoMobile } from './photosessionInfoMobile'
+import Photos from '../../containers/content/photos'
 
 export const MainContent = () => {
   return (
       <main className="main-content">
           <PhotosessionInfo/>
           <PhotosessionInfoMobile/>
+          <Photos/>
       </main>
   )
 };
---
+++ src/app/containers/content/photos.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import photo1 from '../../img/photos/1.png'
+import photo2 from '../../img/photos/2.png'
+import photo3 from '../../img/photos/3.png'
+import photo4 from '../../img/photos/4.png'
+import photo5 from '../../img/photos/5.png'
+import photo6 from '../../img/photos/6.png'
+import photo7 from '../../img/photos/7.png'
+import photo8 from '../../img/photos/8.png'
+import photo9 from '../../img/photos/1.png'
+import photo10 from '../../img/photos/2.png'
+import photo11 from '../../img/photos/3.png'
+import photo12 from '../../img/photos/4.png'
+import photo13 from '../../img/photos/5.png'
+import photo14 from '../../img/photos/6.png'
+import photo15 from '../../img/photos/7.png'
+import photo16 from '../../img/photos/8.png'
+
+const photosArr = [photo1, photo2, photo3, photo4,  photo5,  photo6,  photo7,  photo8,  photo9,  photo10,  photo11,  photo12,  photo13,  photo14,  photo15,  photo16];
+
+export default class Photos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.ref = React.createRef();
+    }
+
+    handleClick = (event) => {
+        this.ref.current.className = this.ref.current.className === 'photos__content photos__content_hidden' ? 'photos__content' : 'photos__content photos__content_hidden';
+
+    };
+
+    render() {
+        const photos = photosArr.map((item, index) => {
+            return <img key={index} src={item} alt="photo" className="photos__image"/>
+        });
+        return (
+            <section className="photos">
+                <h3 className="photos__title">Фотографии пакета:</h3>
+                <div className="photos__content photos__content_hidden" ref={this.ref}>
+                    { photos }
+                </div>
+                <div className="photos__show-hide">
+                    <span className="photos__show-hide-title" onClick={this.handleClick}>
+                          Показать все фото
+                    </span>
+                </div>
+            </section>
+        )
+    }
+}
